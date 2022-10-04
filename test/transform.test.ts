094094a8811b5f17bea33fe535c8409e81e953d1
Luis Herranz <luisherranz@gmail.com>
2022-10-04 16:13:17 +0200

    Add failing custom-elements transform test

--- test/transform.test.ts
+++ test/transform.test.ts
@@ -7,4 +7,9 @@ describe("transform", () => {
     const output = await transform(`<h1>Hello world!</h1>`, { components: { h1: Title } })
     expect(output).toEqual(`<h1 class="ultra">Hello world!</h1>`);
   });
+  it("transforms custom components", async () => {
+    const CustomElement = (props, children) => html`<custom-element class="ultra" ...${props}>${children}</custom-element>`;
+    const output = await transform(`<custom-element>Hello world!</custom-element>`, { components: { "custom-element": CustomElement } })
+    expect(output).toEqual(`<custom-element class="ultra">Hello world!</custom-element>`);
+  })
 });
